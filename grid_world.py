81005742f7c6014b3656d35c2f53a5a46e34b89f
unknown <huahz@hotmail.com>
2018-12-09 11:14:22 +0800

    rename plt with plt2

--- grid_world.py
+++ grid_world.py
@@ -9,7 +9,7 @@ from builtins import range
 import numpy as np
 import pandas as pd 
 import tensorflow as tf 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt2 
 import seaborn as sns 
 #try to add few more libraries here
 
